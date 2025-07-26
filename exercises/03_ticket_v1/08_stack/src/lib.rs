d72aed881685756968fefebc385ca4d90d3fda0e
Vadim Iskuchekov <egregors@pm.me>
2025-07-26 21:03:52 +0000

    .

--- exercises/03_ticket_v1/08_stack/src/lib.rs
+++ exercises/03_ticket_v1/08_stack/src/lib.rs
@@ -1,4 +1,5 @@
 // TODO: based on what you learned in this section, replace `todo!()` with
+
 //  the correct **stack size** for the respective type.
 #[cfg(test)]
 mod tests {
@@ -6,16 +7,16 @@ mod tests {
 
     #[test]
     fn u16_size() {
-        assert_eq!(size_of::<u16>(), todo!());
+        assert_eq!(size_of::<u16>(), 2);
     }
 
     #[test]
     fn i32_size() {
-        assert_eq!(size_of::<i32>(), todo!());
+        assert_eq!(size_of::<i32>(), 4);
     }
 
     #[test]
     fn bool_size() {
-        assert_eq!(size_of::<bool>(), todo!());
+        assert_eq!(size_of::<bool>(), 1);
     }
 }
